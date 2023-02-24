eb0c0677ed337a326aced6d34b76cfc37c6e8bf2
Senapei <115939642+Senapei@users.noreply.github.com>
2023-02-24 13:18:20 +0000

    Add winner function
    The game now displays who the winner is using the "winner" function

--- index.js
+++ index.js
@@ -14,8 +14,28 @@ function setDiceImage() {
 
   let diceValue2 = randomNumber();
   dice2.setAttribute("src", "./img/dice" + diceValue2 + ".png");
+
+  if (diceValue1 > diceValue2) {
+    return 0;
+  } else if (diceValue1 < diceValue2) {
+    return 1;
+  } else if (diceValue1 === diceValue2) {
+    return 2;
+  } else {
+    console.log("error");
+  }
 }
 
-function winner() {}
+function winner() {
+  let diceWinner = setDiceImage();
+  let heading = document.querySelector(".heading");
+  if (diceWinner === 0) {
+    heading.textContent = "Player 1 Wins";
+  } else if (diceWinner === 1) {
+    heading.textContent = "Player 2 Wins";
+  } else if (diceWinner === 2) {
+    heading.textContent = "It's a Draw";
+  }
+}
 
-setDiceImage();
+winner();
